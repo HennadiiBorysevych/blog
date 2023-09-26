f0bb43e6a3e073f3160de589239bd4ed808593cf
Hennadii Borysevych <genuch3@gmail.com>
2023-09-26 14:48:09 +0200

    feat:menubar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import "semantic-ui-css/semantic";
 import "./App.css";
 
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import MenuBar from "./components/MenuBar.jsx";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Route exact path="/" component={MenuBar} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
