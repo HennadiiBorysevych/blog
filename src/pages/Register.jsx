2812ffd9762a401b1dbf5fb4dc6879981adf594e
Hennadii Borysevych <genuch3@gmail.com>
2023-09-26 21:03:19 +0200

    feat: register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { gql } from "@apollo/client";
+import React, { useState } from "react";
+import { Form } from "semantic-ui-react";
+
+
+const REGISTER_USER = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      registerInput: {
+        username: $username
+        email: $email
+        password: $password
+        confirmPassword: $confirmPassword
+      }
+    ) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
+
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
 
-export default Register
+  const onSubmit = () => {};
+
+  return (
+    <Form onSubmit={onSubmit} noValidate>
+      <h1>Register</h1>
+      <Form.Input
+        label="Username"
+        placeholder="Username..."
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        name="username"
+      />
+      <Form.Input
+        label="email"
+        placeholder="email..."
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        name="username"
+      />
+      <Form.Input
+        label="password"
+        placeholder="password..."
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        name="username"
+      />
+      <Form.Input
+        label="confirmPassword"
+        placeholder="confirmPassword..."
+        value={confirmPassword}
+        onChange={(e) => setconfirmPassword(e.target.value)}
+        name="username"
+      />
+    </Form>
+  );
+};
+
+
+export default Register;
